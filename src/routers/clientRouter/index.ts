fe287d7fd9b52f9ca926d482bac448e08fe680ad
kukechen <chen.huan@infoloop.cn>
2022-08-12 19:06:43 +0800

    ~

--- src/routers/clientRouter/index.ts
+++ src/routers/clientRouter/index.ts
@@ -138,10 +138,10 @@ router.post('/get_one_task',
         const gameRole = await gameRoleDao.getGameRoleByQuery({gameId})
         const groupId = gameRole.groupId
         const targetGameRole = await gameRoleDao.getGameRoleByQuery({groupId, work, status: '空闲'})
-        if(work == '挖图' || work == '接货') {
-            await gameRoleDao.updateGameRoleStatus(targetGameRole.gameId, '忙碌')
-        }
         if(targetGameRole) {
+            if(work == '挖图' || work == '接货') {
+                await gameRoleDao.updateGameRoleStatus(targetGameRole.gameId, '忙碌')
+            }
             res.json( {status: 0, data: targetGameRole, gameId: targetGameRole.gameId})
         }else {
             res.json( {status: -1})
