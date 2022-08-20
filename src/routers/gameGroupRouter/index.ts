50789ac35b8c9249db7c5167777cf61563605866
kukechen <chen.huan@infoloop.cn>
2022-08-20 18:04:23 +0800

    ~

--- src/routers/gameGroupRouter/index.ts
+++ src/routers/gameGroupRouter/index.ts
@@ -15,6 +15,9 @@ router.post('/add_game_group', async function (req:Request<any, any, TCreateGame
         // @ts-ignore
         body.userId = req.loginUser.id
     }
+    if(!body.priceConfig) {
+        body.priceConfig = '50铁=2,60铁=6,70铁=11,50书=0.5,60书=9,70书=2,宝图=2.5,红玛瑙=7,黑宝石=13,光芒石=4,月亮石=5,太阳石=6,50环=1.7,60环=17,70环=9,内丹=30,兽决=65,金柳露=6,定魂珠=130,金刚石=130,避水珠=5,龙鳞=40,夜光珠=90'
+    }
     const gameGroup = await gameGroupDao.saveGameGroup(body)
     if(gameGroup) {
         return res.json({
