c234a46e6ce6cf92f8c54a8d89d5903391b8fb15
kukechen <chen.huan@infoloop.cn>
2022-08-29 10:35:22 +0800

    ~

--- src/routers/clientRouter/index.ts
+++ src/routers/clientRouter/index.ts
@@ -120,7 +120,7 @@ export type TClinetStartTaskRequest = {
         const groupId = gameRole.groupId
         const targetGameRole = await gameRoleDao.getGameRoleByQuery({groupId, work, status: '空闲'})
         if(targetGameRole) {
-            if(work == '挖图' || work == '接货') {
+            if(work == '挖图') {
                 await gameRoleDao.updateGameRoleStatus(targetGameRole.gameId, '忙碌')
             }
             res.json( {status: 0, data: targetGameRole, gameId: targetGameRole.gameId})
